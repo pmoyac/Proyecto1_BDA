d8bcaee02b804182229c6329dcea3a9ccb1c1c62
pmoyac <48139060+pmoyac@users.noreply.github.com>
2024-02-12 17:52:01 -0700

    Carga inicial

---
+++ src/main/java/com/mycompany/proyecto1_bda/Proyecto1_BDA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto1_bda;
+
+/**
+ *
+ * @author Pedro
+ */
+public class Proyecto1_BDA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
