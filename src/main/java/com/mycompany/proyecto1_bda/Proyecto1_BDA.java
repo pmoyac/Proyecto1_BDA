5779bc6000addabf2feecbc0a0b4a69d8b6f8879
pmoyac <48139060+pmoyac@users.noreply.github.com>
2024-02-15 15:40:17 -0700

    prueba agregar cuenta

--- src/main/java/com/mycompany/proyecto1_bda/Proyecto1_BDA.java
+++ src/main/java/com/mycompany/proyecto1_bda/Proyecto1_BDA.java
@@ -1,12 +1,36 @@
 package com.mycompany.proyecto1_bda;
 
+import datos.ClienteDAO;
+import datos.ConexionBD;
+import datos.CuentaDAO;
+import excepciones.PersistenciaException;
+import interfaces.ICliente;
+import interfaces.IConexion;
+import interfaces.ICuenta;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import objetos.Cuenta;
+
 /**
  * @author Pedro Moya, Adriana Gutiérrez
  */
 public class Proyecto1_BDA {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws PersistenciaException {
         System.out.println("Hello !");
         System.out.println("este es jovani");
+        
+        
+        String cadenaConexion = "jdbc:mysql://localhost:3306/banco";
+        String user = "root";
+        String password = "moya";
+        
+        IConexion baseDatos= new ConexionBD(cadenaConexion, user, password);
+        
+//        ICuenta cu = new CuentaDAO(baseDatos);
+//        Cuenta c1= new Cuenta(3000, new Date(120, 6, 12));
+//        cu.agregarCuenta(c1);
     }
 }
