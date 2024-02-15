89c8bf0e54213a81149c2924235cdb37218ec1ee
adrianag4 <adriana.gutierrez235633@potros.itson.edu.mx>
2024-02-15 13:33:08 -0700

    registrar, buscar cliente

--- src/main/java/com/mycompany/proyecto1_bda/Proyecto1_BDA.java
+++ src/main/java/com/mycompany/proyecto1_bda/Proyecto1_BDA.java
@@ -1,12 +1,42 @@
 package com.mycompany.proyecto1_bda;
 
+import datos.ClienteDAO;
+import datos.ConexionBD;
+import excepciones.PersistenciaException;
+import interfaces.ICliente;
+import interfaces.IConexion;
+import java.sql.Date;
+import objetos.Cliente;
+
 /**
  * @author Pedro Moya, Adriana Gutiérrez
  */
 public class Proyecto1_BDA {
 
     public static void main(String[] args) {
-        System.out.println("Hello !");
-        System.out.println("este es jovani");
+
+        String cadenaConexion = "jdbc:mysql://localhost:3306/banco";
+        String user = "root";
+        String password = "1234";
+        
+        try {
+            IConexion baseDatos = new ConexionBD(cadenaConexion, user, password);
+            Cliente cliente = new Cliente("adriana","gutierrez","robles","1234",new Date(2003,04,04),"calle","178","f");
+            ICliente daoCliente = new ClienteDAO(baseDatos);
+            Cliente agregado = daoCliente.registrarCliente(cliente);
+            
+            System.out.println("Cliente registrado");
+            System.out.println(agregado.toString());
+            
+        } catch (PersistenciaException e) {
+            System.out.println("Hubo un error");
+            System.out.println(e.getStackTrace());
+        }
+        
+        
+        
+        
     }
+    
+    
 }
